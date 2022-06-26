3a1c9806d1f220b0f504a1b270c25fd5afc8bf50
fenglong_ng <fenglong1502@hotmail.com>
2022-06-26 22:53:51 +0800

    form sg set up code

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 const express = require('express');
 const app = express();
 
-// Testing on get api
+const port = 8080;
+
+// Testing serber Get api
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
-const port = 3000;
-app.listen(port, console.log(`Server is listening to port ${port}...`));
+
+// Instantiating formsg-sdk without parameters default to using the package's
+// production public signing key.
+const formsg = require('@opengovsg/formsg-sdk')()
+
+// This is where your domain is hosted, and should match
+// the URI supplied to FormSG in the form dashboard
+const POST_URI = 'https://my-domain.com/submissions'
+
+// Your form's secret key downloaded from FormSG upon form creation
+const formSecretKey = process.env.FORM_SECRET_KEY
+
+// Set to true if you need to download and decrypt attachments from submissions
+const HAS_ATTACHMENTS = false
+
+app.post(
+  '/submissions',
+  // Endpoint authentication by verifying signatures
+  function (req, res, next) {
+    try {
+      formsg.webhooks.authenticate(req.get('X-FormSG-Signature'), POST_URI)
+      // Continue processing the POST body
+      return next()
+    } catch (e) {
+      return res.status(401).send({ message: 'Unauthorized' })
+    }
+  },
+  // Parse JSON from raw request body
+  express.json(),
+  // Decrypt the submission
+  async function (req, res, next) {
+    // If `verifiedContent` is provided in `req.body.data`, the return object
+    // will include a verified key.
+    const submission = HAS_ATTACHMENTS
+      ? await formsg.crypto.decryptWithAttachments(formSecretKey, req.body.data)
+      : formsg.crypto.decrypt(formSecretKey, req.body.data)
+
+    // If the decryption failed, submission will be `null`.
+    if (submission) {
+      // Continue processing the submission
+    } else {
+      // Could not decrypt the submission
+    }
+  }
+)
+
+app.listen(port, () => console.log(`Running on port ${port}`))
